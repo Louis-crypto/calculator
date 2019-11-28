8bd88f6a07635ec21e753baae86e1a4d793fc81d
Louis-crypto <kinglouis745@gmail.com>
2019-11-28 12:42:32 +0300

    introduce js code for calculator

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var add = function (number1, number2){
+  return number1 + number2;
+}
+
+var subtraction = function (number1, number2){
+  return number1 - number2;
+}
+
+var multiply = function (number1, number2){
+  return number1 - number2;
+}
+
+var divide = function (number1, number2){
+  return number1 - number2;
+}
+var number1 = parseInt(prompt("Enter first number"));
+var number2 = parseInt(prompt("Enter second number"));
+alert(number1 + number2);
